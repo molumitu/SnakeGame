877207040c1833cb6d7dd1a8b90a4bf2dd931e1b
molumitu <zgj_thu17@163.com>
2022-02-24 20:20:07 +0800

    Add ray.tune

--- rllib_main.py
+++ rllib_main.py
@@ -1,11 +1,26 @@
 from ray.rllib.agents.dqn import DQNTrainer
+import ray.tune
 from env import SnakeGame
 config = {
     "env": SnakeGame,
     "seed":73,
     "framework":"torch",
+
+    "prioritized_replay": ray.tune.grid_search([False, True]),
+    "dueling": ray.tune.grid_search([False, True]),
+    "double_q": ray.tune.grid_search([False, True]),
+    # "num_atoms": 0,
 }
 
 trainer = DQNTrainer(config)
-while True:
-    trainer.train()
+ray.tune.run(
+    DQNTrainer,
+    config=config,
+    stop={
+        "episode_reward_mean": 300,
+        "agent_timesteps_total": 200000,
+    },
+    checkpoint_at_end=True,
+    verbose=1,
+    progress_reporter=ray.tune.CLIReporter(print_intermediate_tables=True)
+)
