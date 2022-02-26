60a6d5a06c5e00e196e02c93a6d529b5171c8eab
molumitu <zgj_thu17@163.com>
2022-02-26 16:53:14 +0800

    Fix evaluation

--- evaluation.py
+++ evaluation.py
@@ -1,10 +1,10 @@
 from env import SnakeGame
 import ray.rllib.agents.dqn as dqn
-PATH = R"C:/Users/zgj_t/ray_results/DQNTrainer_2022-02-25_16-32-16/DQNTrainer_RacingEnv_70340_00000_0_double_q=False,dueling=False_2022-02-25_16-32-16/checkpoint_000085/checkpoint-85"
+PATH = R"C:/Users/zgj_t/ray_results/DQN_2022-02-26_16-34-32/DQN_SnakeGame_ebfd5_00000_0_double_q=False,dueling=False_2022-02-26_16-34-32/checkpoint_000140/checkpoint-140"
 config = {
     "env": SnakeGame,
-    "seed":73,
-    "framework":"tf2",
+    "seed":5,
+    "framework":"torch",
     "prioritized_replay": True,
     "dueling": False,
     "double_q": False,
@@ -16,10 +16,12 @@ trainer.restore(PATH)
 episode_reward = 0
 done = False
 env = SnakeGame()
+env.render_init()
 obs = env.reset()
 while not done:
-    action = trainer.compute_action(obs)
+    action = trainer.compute_single_action(obs)
     obs, reward, done, info = env.step(action)
-    env.render(action)
+    env.render_update()
     episode_reward += reward
+env.render_complete()
 print(episode_reward)
